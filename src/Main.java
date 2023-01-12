56e1c13af2b4de2ec7f85ff64d8b703a1625350b
jrgdonoso <jrg.donoso@gmail.com>
2023-01-12 14:11:13 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Integer dividendo=2;
+        Integer divisor=0;
+
+        Integer cociente= dividendo/divisor;
+
+
+        System.out.println("El resultado es "+cociente);
+    }
+
+
+
+
+
+}
